60214b1d9e6c90216076f03853608cd2aedaa1f6
chiranjith <chiranjith@users.noreply.github.com>
2025-02-16 21:16:05 -0600

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/builtFromScratch/", // Replace with your repository name
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor"; // Moves dependencies to a separate file
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase limit (optional)
-  },
+  base: "/builtFromScratch/", // Ensure this is correct
 });
